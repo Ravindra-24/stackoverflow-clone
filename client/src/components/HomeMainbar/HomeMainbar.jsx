7b1da512c8308a4985597b8e91e67dc5c996d055
Ravindra Pawar <ravindra.pawar.mit@gmail.com>
2023-02-10 13:04:04 +0530

    stackOverflow Home Mainbar UI

--- client/src/components/HomeMainbar/HomeMainbar.jsx
+++ client/src/components/HomeMainbar/HomeMainbar.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
+import {Link , useLocation} from 'react-router-dom'
+
+import './HomeMainbar.css'
+import Questions from './Questions'
+import QuestionList from './QuestionList'
 
 const HomeMainbar = () => {
+  var questionsList = [{ 
+        _id: 1,
+        upVotes: 3,
+        downVotes: 2,
+        noOfAnswers: 2,
+        questionTitle: "What is a function?",
+        questionBody: "It meant to be",
+        questionTags: ["java", "node js", "react js", "mongo db", "express js"],
+        userPosted: "mano",
+        userId: 1,
+        askedOn: "jan 1",
+        answer: [{
+            answerBody: "Answer",
+            userAnswered: 'kumar',
+            answeredOn: "jan 2",
+            userId: 2,
+        }]
+    },{ 
+        _id: 2,
+        upVotes: 3,
+        downVotes: 2,
+        noOfAnswers: 0,
+        questionTitle: "What is a function?",
+        questionBody: "It meant to be",
+        questionTags: ["javascript", "R", "python"],
+        userPosted: "mano",
+        askedOn: "jan 1",
+        userId: 1,
+        answer: [{
+            answerBody: "Answer",
+            userAnswered: 'kumar',
+            answeredOn: "jan 2",
+            userId: 2,
+        }]
+    },{ 
+        _id: 3,
+        upVotes: 3,
+        downVotes: 2,
+        noOfAnswers: 0,
+        questionTitle: "What is a function?",
+        questionBody: "It meant to be",
+        questionTags: ["javascript", "R", "python"],
+        userPosted: "mano",
+        askedOn: "jan 1",
+        userId: 1,
+        answer: [{
+            answerBody: "Answer",
+            userAnswered: 'kumar',
+            answeredOn: "jan 2",
+            userId: 2,
+        }]
+    }]
+
+    const location = useLocation()
   return (
-    <div>
-      
+    <div className='main-bar'>
+      <div className='main-bar-header'>
+        {
+          location.pathname === '/' ? <h1>Top Question</h1> : <h1>All Question</h1>
+        }
+        <Link to='/AskQuestions' className='ask-btn'>Ask Questions</Link>
+      </div>
+      <div>
+        {
+          questionsList === null ?
+          <p>Loading...</p> :
+          <>
+          <p>{questionsList.length} Questions</p>
+          <QuestionList questionsList={questionsList}/>
+          </>
+        }
+      </div>
     </div>
   )
 }
