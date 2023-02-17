2a0ad0f2a0d2c61bccee3aee526ad61e7c8cf060
Ravindra Pawar <ravindra.pawar.mit@gmail.com>
2023-02-17 14:58:00 +0530

    URL Update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: 'http://localhost:5000'}) 
+const API = axios.create({baseURL: 'https://http-nodejs-production-5b32.up.railway.app'}) 
 
     API.interceptors.request.use((req) => {
         if(localStorage.getItem('profile')) {
